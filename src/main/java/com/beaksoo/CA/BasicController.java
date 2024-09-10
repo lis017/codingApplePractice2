f4a4a098256d28ea816428de5feb685c67a58a42
lis017 <minhyeok12.dev@gmail.com>
2024-09-10 11:11:04 +0900

    root module 이름 바꿨다가 쏟아지는 오류더미에 3~4시간 시간박고 결국은 항복하고
    새로 프로젝트 만들어서 하는 첫 push, commit

---
+++ src/main/java/com/beaksoo/CA/BasicController.java
@@ -0,0 +1,16 @@
+package com.beaksoo.CA;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class BasicController {
+    @GetMapping("/")
+    @ResponseBody
+    String hello(){
+        return "ㅎㅇ";
+    }
+
+
+}
